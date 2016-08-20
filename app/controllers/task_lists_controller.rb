29023bd84914ebb3a74a0fed40437a3736644289
Felipe Lima <felipepassine@gmail.com>
2016-08-20 15:56:09 -0300

    add tasklist controller.

---
+++ app/controllers/task_lists_controller.rb
@@ -0,0 +1,7 @@
+class TaskListsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @task_lists = current_user.task_lists
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root "task_lists#index"
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :task_list, only: [:index]
 end
